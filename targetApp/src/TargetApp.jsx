5faa70ba47b44790958d35885c4192b13d52fe02
Louis-Mascari <Lmascari16@gmail.com>
2024-03-07 21:12:48 -0500

    feat - capturing fetch requests and able to log array of network requests made so far with button click
    Co-authored-by: Erik Wiens <erik.wiens@gmail.com>

--- targetApp/src/TargetApp.jsx
+++ targetApp/src/TargetApp.jsx
@@ -3,14 +3,42 @@ import { record, getRecordConsolePlugin } from "rrweb";
 
 const TargetApp = () => {
   let events = [];
+  let networkRequests = [];
+
+  const originalFetch = window.fetch;
+  window.fetch = function (input, init) {
+    // Your Fetch interception logic here
+    const obj = {
+      url: input,
+      method: init.method,
+      headers: init.headers,
+      body: init.body.slice(0, 30),
+      type: "FETCH",
+      timeStamp: Date.now(),
+    };
+    const result = originalFetch.apply(this, arguments);
+
+    result
+      .then((res) => {
+        obj.status = res.status;
+        networkRequests.push(obj);
+        return res;
+      })
+      .catch((error) => {
+        console.error("Fetch Error: ", error);
+      });
+
+    return result;
+  };
 
   useEffect(() => {
     const stopRecording = record({
       emit(event) {
         events.push(event);
-        const defaultLog = console.log["__rrweb_original__"]
-          ? console.log["__rrweb_original__"]
-          : console.log;
+        //
+        //   const defaultLog = console.log["__rrweb_original__"]
+        //     ? console.log["__rrweb_original__"]
+        //     : console.log;
       },
       plugins: [getRecordConsolePlugin()],
     });
@@ -56,6 +84,7 @@ const TargetApp = () => {
       <button onClick={() => console.error(new Error())}>
         Click for error
       </button>
+      <button onClick={() => console.log(networkRequests)}>Log Requests</button>
     </>
   );
 };
